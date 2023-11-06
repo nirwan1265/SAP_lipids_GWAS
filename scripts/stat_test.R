cd0add9c4c47e73c9b9db6877050aab65e05a691
nirwan1265 <tandukar.nirwan@gmail.com>
2023-11-06 07:44:57 -0500

    update

--- scripts/stat_test.R
+++ scripts/stat_test.R
@@ -2,57 +2,52 @@
 common_compounds <- intersect(colnames(PC_A), colnames(PC_B))
 
 # Subset the data frames to include only common compounds
-common_compounds_A <- PC_A[PC_A %in% common_compounds]
-common_compounds_B <- PC_B[PC_B$Compound_Prefix %in% common_compounds, ]
+common_compounds_A <- PC_A[,common_compounds]
+common_compounds_B <- PC_B[,common_compounds]
 
 
 str(common_compounds_A)
+str(common_compounds_B)
 
 
-# Initialize p-values vector
-p_values <- numeric(length(common_compounds))
-
-# Initialize p-values vector
-p_values <- numeric(nrow(common_compounds_A))
-
-# Perform statistical tests for each compound row-wise
-for (i in 1:nrow(common_compounds_A)) {
-  data_A <- as.numeric(common_compounds_A[i, -1])
-  data_B <- as.numeric(common_compounds_B[i, -1])
+#Check if column names match
+if(all(names(common_compounds_A) == names(common_compounds_B))) {
+  # Initialize a data frame to store test results
+  results <- data.frame(Column = character(0), p_value = numeric(0))
   
-  # Perform a statistical test (e.g., Wilcoxon rank-sum test) for this compound
-  test_result <- wilcox.test(data_A, data_B)
-  p_values[i] <- test_result$p.value
+  # Loop through column names and perform t-tests
+  for (col_name in names(common_compounds_A)) {
+    t_test_result <- t.test(common_compounds_A[[col_name]], common_compounds_B[[col_name]])
+    results <- rbind(results, data.frame(Column = col_name, p_value = t_test_result$p.value))
+  }
+  
+  # Print or analyze the results
+  print(results)
+} else {
+  print("Column names do not match between the two data frames.")
 }
 
-# Add the Compound_Prefix column to the p-values vector
-p_values <- cbind(Compound_Prefix = common_compounds_A$Compound_Prefix, P_Value = p_values)
+# Sort the results data frame by p-value in ascending order
+sorted_results <- results[order(results$p_value), ]
 
-str(p_values)
-# Convert p_values into a data frame
-p_values <- as.data.frame(p_values, stringsAsFactors = FALSE)
+# View the sorted results
+print(sorted_results)
 
-# Sort the p-values data frame by P_Value (ascending order)
-p_values <- p_values[order(as.numeric(p_values$P_Value)), ]
+# Significance level (adjust as needed)
+significance_level <- 0.05
 
+# Filter the sorted results to include only statistically significant columns
+significant_results <- sorted_results[sorted_results$p_value < significance_level, ]
 
-# Differences between the concentration
-# Create a new dataframe to store the differences
-differences <- common_compounds_A
+# View the significant results
+print(significant_results)
 
-# Exclude the 'Compound_Prefix' column from both dataframes
-common_compounds_A_values <- common_compounds_A[, -1]
-common_compounds_B_values <- common_compounds_B[, -1]
+# Loop through the significant columns and calculate the mean difference
+for (i in 1:nrow(significant_results)) {
+  col_name <- significant_results$Column[i]
+  mean_diff <- mean(common_compounds_A[[col_name]]) - mean(common_compounds_B[[col_name]])
+  significant_results$Mean_Difference[i] <- mean_diff
+}
 
-# Calculate row-wise averages for common_compounds_A and common_compounds_B
-average_A <- rowMeans(common_compounds_A[, -1])
-average_B <- rowMeans(common_compounds_B[, -1])
-
-# Create a new dataframe to store the differences
-differences <- data.frame(Compound_Prefix = common_compounds_A$Compound_Prefix, Difference = average_A - average_B)
-
-# Find unique compounds in data frame A
-unique_compounds_A <- setdiff(PC_A$Compound_Prefix, PC_B$Compound_Prefix)
-
-# Find unique compounds in data frame B
-unique_compounds_B <- setdiff(PC_B$Compound_Prefix, PC_A$Compound_Prefix)
+# View the updated significant_results data frame
+print(significant_results)
